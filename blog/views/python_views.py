6b98f9a3dcc1f4141b9e118871cba9ffe45a43f7
md shariful islam <sharifulcsehstu@gmail.com>
2019-11-16 14:58:09 +0600

    fixing

--- blog/views/python_views.py
+++ blog/views/python_views.py
@@ -7,21 +7,24 @@ from rest_framework.views import APIView
 from blog.models import Post
 
 
-class StringOperation(APIView):
+class BuiltInFunction(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'python/string_operation.html'
+    template_name = 'python/built_in_function.html'
 
     def get(self, request):
         post = Post.objects.get(pk=3)
         previous = Post.objects.get(pk=2)
+        next = Post.objects.get(pk=3)
+
         if post:
-            return Response({'post': post}, status=status.HTTP_200_OK)
+            return Response({'post': post, 'previous': previous, 'next': next}, status=status.HTTP_200_OK)
         return Response({'post': post}, status=status.HTTP_200_OK)
 
 
-class ListOperation(APIView):
+class StringOperation(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'python/list_operation.html'
+    template_name = 'python/string_operation.html'
+
 
     def get(self, request):
         post = Post.objects.get(pk=2)
@@ -32,9 +35,10 @@ class ListOperation(APIView):
         return Response({'post': post}, status=status.HTTP_200_OK)
 
 
-class BuiltInFunction(APIView):
+class ListOperation(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'python/built_in_function.html'
+    template_name = 'python/list_operation.html'
+
 
     def get(self, request):
         post = Post.objects.get(pk=1)
